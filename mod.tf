3d634b4aa0bcd2299eb079503f36cf81018750c0
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2020-10-02 13:56:16 +0300

    Create mod.tf

---
+++ mod.tf
@@ -0,0 +1,8 @@
+module "ik-modules" {
+	source  = "2bc4d33c2563.test-env.scalr.com/env-svrcnchebt61e30/ik-modules/aws"
+	version = "1.3.1"
+
+	# Set 1 required variable below.
+
+	text_var = "string"
+}
