180ab07c9fd486ba5e040086efd49679ec874c46
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2020-10-05 12:43:56 +0300

    Update mod.tf

--- mod.tf
+++ mod.tf
@@ -1,5 +1,5 @@
 module "ik-modules" {
-	source  = "0202d5c9db99.test-env.scalr.com/env-svrcnchebt61e30/ik-modules/aws"
+	source  = "2bc4d33c2563.test-env.scalr.com/env-svrcnchebt61e30/ik-modules/aws"
 	version = "1.3.1"
 
 	# Set 1 required variable below.
