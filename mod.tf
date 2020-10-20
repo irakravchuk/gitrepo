f1adb34ebf7500fa3a37af09870ce39086c83d1c
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2020-10-20 11:17:27 +0300

    Update mod.tf

--- mod.tf
+++ mod.tf
@@ -1,7 +1,6 @@
-module "ik-modules" {
-	source  = "irak.likehosted.scalr.com/acc-t55gcjlfbkm10u0/ik-modules/aws"
-	version = "1.3.1"
-	# Set 1 required variable below.
+module "ira" {
+	source  = "irak.likehosted.scalr.com/acc-t55gcjlfbkm10u0/ira/aws"
+	version = "1.1.2"
 
 	text_var = "string"
 }
