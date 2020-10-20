2c1506efb15713dee8b81d7a40710efe0f1380c5
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2020-10-20 19:13:45 +0300

    Create main.tf

---
+++ 16619/main.tf
@@ -0,0 +1,11 @@
+provider "oraclepaas" {
+  version = "1.5.3"
+}
+provider "google" {}
+provider "google-beta" {}
+provider "azurerm" {}
+provider "kubernetes" {}
+provider "helm" {}
+provider "aws" {
+  version = "3.11.0"
+}
