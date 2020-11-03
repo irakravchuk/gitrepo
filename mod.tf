91ce42de9dce88112a9d10945504d9f3e2a8e7a5
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2020-11-03 20:04:31 +0200

    Update mod.tf

--- mod.tf
+++ mod.tf
@@ -1,5 +1,5 @@
 module "ira" {
-	source  = "57654ed06917.test-env.scalr.com/acc-svrcncgh453bi8g/ira/aws"
+	source  = "e63bb2393225.test-env.scalr.com/global/ira/aws"
 	version = "1.1.2"
 
 	#text_var = "string"
