d5d2f05d1927f94e81c4df38fa6a760180b0c891
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2020-11-12 09:27:27 +0200

    Update mod.tf

--- mod.tf
+++ mod.tf
@@ -1,9 +1,9 @@
 module "ira" {
 	source  = "irak.likehosted.scalr.com/env-t69evhm72a44dqo/ira/aws"
 	version = "1.1.2"
-	text_var = "string"
+	#text_var = "string"
 }
 variable "text_var" {
- #type = string
+ type = string
  default = null
 }
