08ee82e3ffd8b0a934636334d16df78578a4669c
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2020-12-09 16:16:52 +0200

    Update mod.tf

--- mod.tf
+++ mod.tf
@@ -1,5 +1,5 @@
 module "ik-modules" {
-	source  = "3a797a70.devel.scalr.com/env-t7irvs6nvk2b9n0/ik-modules/aws"
+	source  = "mainiacp.org9.te.scalr-labs.net/global/ik-modules/aws"
 	version = "1.3.1"
 	#text_var = "string"
 }
