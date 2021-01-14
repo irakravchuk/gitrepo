891043cde40bf77b10112f4c69215abb958add33
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2021-01-14 14:19:12 +0200

    Update mod.tf

--- mod.tf
+++ mod.tf
@@ -2,10 +2,6 @@ module "ik-modules" {
 	source  = "scalr-iacp.scalr.io/org-sfuari395m7sck1/ik-modules/aws"
 	version = "1.3.1"
 
-	# Set 1 required variable below.
-
-	text_var = <value>
-}
 	#text_var = "string"
 }
 variable "text_var" {
