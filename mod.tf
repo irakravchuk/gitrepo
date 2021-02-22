5685be5dcd5de9f47d8c556b979ad914002b7664
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2021-02-22 12:18:00 +0200

    Update mod.tf

--- mod.tf
+++ mod.tf
@@ -1,6 +1,6 @@
-module "ira-master" {
-	source  = "cbb883747b46.test-env.scalr.com/env-svrcnchebt61e30/ira-master/aws"
-	version = "1.1.1"
+module "tftest" {
+	source  = "56caab2fd335.test-env.scalr.com/acc-svrcncgh453bi8g/tftest/aws"
+	version = "0.0.5"
 
 	#text_var = "string"
 }
