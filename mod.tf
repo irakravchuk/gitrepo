4b662a68646137fc1e8cbd79a67f2ebd6cddeac4
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2021-03-09 14:23:59 +0200

    Update mod.tf

--- mod.tf
+++ mod.tf
@@ -1,6 +1,6 @@
-module "ik-modules" {
-	source  = "4665829274b5.test-env.scalr.com/global/ik-modules/aws"
-	version = "1.3.1"
+module "ik-master" {
+	source  = "scalr-iacp.scalr.io/org-sfuari395m7sck1/ik-master/yandex"
+	version = "0.0.2"
 
 	#text_var = "string"
 }
