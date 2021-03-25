c7b10ea0630ec78e723c23c79597dea677af9d81
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2021-03-25 11:12:33 +0200

    Update mod.tf

--- mod.tf
+++ mod.tf
@@ -1,6 +1,6 @@
-module "ik-modules" {
-	source  = "1794f0194149.test-env.scalr.com/global/ik-modules/aws"
-	version = "1.3.1"
+module "tftest" {
+	source  = "irak.org5.te.scalr-labs.net/env-tcv9m0fhhns4s7o/tftest/aws"
+	version = "0.0.5"
 	#text_var = "string"
 }
 variable "text_var" {
