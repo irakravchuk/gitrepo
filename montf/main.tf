4994b9d2be752e003722ca95c68ab6d28b330030
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2021-03-25 16:19:43 +0200

    Update main.tf

--- montf/main.tf
+++ montf/main.tf
@@ -1,10 +1,19 @@
+
+provider "aws" {
+  #region     = var.region
+ # region = ""
+  region = "us-east-1"
+  #access_key = "${var.scalr_aws_access_key}"
+  #secret_key = "${var.scalr_aws_secret_key}" 
+}
+
 module "web_server_sg" {
   source = "terraform-aws-modules/security-group/aws//modules/http-80"
 
   name        = "web-server"
   description = "Security group for web-server with HTTP ports open within VPC"
   vpc_id      = "vpc-12345678"
-  region = "us-east-1"
+ 
 
   ingress_cidr_blocks = ["10.10.0.0/16"]
 }
