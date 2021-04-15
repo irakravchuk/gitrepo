eb0fc73a382ef6ba2b5197e41e6b92777037b79d
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2021-04-15 11:03:12 +0300

    Update mod.tf

--- mod.tf
+++ mod.tf
@@ -1,6 +1,6 @@
 module "tftest" {
 	source  = "ddd03d260884.test-env.scalr.com/env-svrcnchebt61e30/tftest/aws"
-	version = "0.1.3"
+	version = "0.1.2"
 	#text_var = "string"
 }
 variable "text_var" {
